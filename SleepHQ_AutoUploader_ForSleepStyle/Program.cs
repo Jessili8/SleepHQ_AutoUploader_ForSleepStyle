380f8023dd6ccde02f1256673e88ff4743b2cceb
Jessili8 <jessiewww2@gmail.com>
2024-08-24 17:49:34 +0800

    加入專案檔案。

---
+++ SleepHQ_AutoUploader_ForSleepStyle/Program.cs
@@ -0,0 +1,100 @@
+﻿using Newtonsoft.Json;
+using SleepHQ_AutoUploader_ForSleepStyle;
+using System.Security.Cryptography;
+
+internal class Program
+{
+    public static string? DeviceId { get; set; }
+    public static string? ClientId { get; set; }
+    public static string? ClientSecret { get; set; }
+    public static string? SubPath { get; set; }
+    public static string? DirPath { get; set; }
+
+    private static void Main()
+    {
+        EnvironmentVariables();
+        CheckUSBDrive();
+    }
+
+    private static void CheckUSBDrive()
+    {
+        DriveInfo[] drives = DriveInfo.GetDrives();
+        List<DriveInfo> usbDrives = drives.Where(drive => drive.DriveType == DriveType.Removable && drive.IsReady).ToList();
+
+        if (usbDrives.Count == 0)
+        {
+            Console.WriteLine("No external Drive found. ");
+            return;
+        }
+
+        foreach (DriveInfo drive in usbDrives)
+        {
+            Console.WriteLine($"USB Drive found: {drive.Name}");
+            string folderPath = Path.Combine(drive.Name, "FPHCARE");
+
+            if (Directory.Exists(folderPath))
+            {
+                Console.WriteLine($"FPHCARE folder found in：{folderPath}");
+                Dictionary<string, List<string>> CollectedFiles = CollectFiles(folderPath);
+                List<Dictionary<string, string>> orderedImportFiles = CollectedFiles["path"].Select((t, i) => new Dictionary<string, string>
+                {
+                    { "path", t },
+                    { "name", CollectedFiles["name"][i] },
+                    { "content_hash", CollectedFiles["content_hash"][i] }
+                }).ToList();
+                string finalImportFilesJson = JsonConvert.SerializeObject(orderedImportFiles);
+                string authorization = SleepHQClientService.GetAccessToken(ClientId, ClientSecret);
+                string teamId = SleepHQClientService.GetTeamId(authorization);
+                string importId = SleepHQClientService.ReserveImportId(teamId, authorization);
+                Console.WriteLine($"Reserve Team ID: {importId} successfully. ");
+                SleepHQClientService.UploadFiles(importId, authorization, orderedImportFiles, DirPath);
+                SleepHQClientService.ProcessImportedFiles(importId, authorization);
+                Console.WriteLine($"Processing Files...");
+                Task.Delay(30000);
+                SleepHQClientService.CheckImportedFiles(importId, authorization);
+            }
+            else
+            {
+                Console.WriteLine($"We cannot found FPHCARE folder in {drive.Name}.");
+            }
+        }
+    }
+
+    static Dictionary<string, List<string>> CollectFiles(string folderPath)
+    {
+        IEnumerable<FileInfo> allFiles = Directory.GetFiles(folderPath, "*", SearchOption.AllDirectories)
+            .Where(f => !Path.GetFileName(f).StartsWith('.'))
+            .Select(f => new FileInfo(f));
+
+        Dictionary<string, List<string>> importFiles = new Dictionary<string, List<string>>
+            {
+                { "path", [] },
+                { "name", [] },
+                { "content_hash", [] }
+            };
+
+        foreach (var file in allFiles)
+        {
+            string finalPath = Path.GetFullPath(file.DirectoryName);
+            importFiles["path"].Add(finalPath);
+            importFiles["name"].Add(file.Name);
+            importFiles["content_hash"].Add(CalculateMD5(file.FullName));
+        }
+
+        return importFiles;
+    }
+
+    private static string CalculateMD5(string filename)
+    {
+        using MD5 md5 = MD5.Create();
+        using FileStream stream = File.OpenRead(filename);
+        byte[] hash = md5.ComputeHash(stream);
+        return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+    }
+
+    private static void EnvironmentVariables()
+    {
+        ClientId = Environment.GetEnvironmentVariable("CLIENT_ID");
+        ClientSecret = Environment.GetEnvironmentVariable("CLIENT_SECRET");
+    }
+}
---
+++ SleepHQ_AutoUploader_ForSleepStyle/SleepHQClientService.cs
@@ -0,0 +1,178 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace SleepHQ_AutoUploader_ForSleepStyle
+{
+    public class SleepHQClientService
+    {
+        public static string GetAccessToken(string clientId, string clientSecret)
+        {
+            string url = "https://sleephq.com/oauth/token";
+            Dictionary<string, string> payload = new()
+            {
+                { "client_id", clientId },
+                { "client_secret", clientSecret },
+                { "grant_type", "password" },
+                { "scope", "read write" }
+            };
+
+            using HttpClient httpClient = new();
+            try
+            {
+                var response = httpClient.PostAsync(url, new FormUrlEncodedContent(payload)).Result;
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine("Get access token successfully.");
+
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+                var jsonResponse = JObject.Parse(responseBody);
+                return "Bearer " + jsonResponse["access_token"].ToString();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Access Denied：{e.Message}");
+                Environment.Exit(1);
+                return null;
+            }
+        }
+
+        public static string GetTeamId(string authorization)
+        {
+            string url = "https://sleephq.com/api/v1/me";
+            using HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Add("Authorization", authorization);
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            try
+            {
+                HttpResponseMessage response = httpClient.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+
+                string responseBody = response.Content.ReadAsStringAsync().Result;
+                JObject jsonResponse = JObject.Parse(responseBody);
+                return jsonResponse["data"]["current_team_id"].ToString();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Failed to retrieve TeamId：{e.Message}");
+                Environment.Exit(1);
+                return null;
+            }
+        }
+
+        public static string ReserveImportId(string teamId, string authorization)
+        {
+            string url = $"https://sleephq.com/api/v1/teams/{teamId}/imports";
+            using HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Add("Authorization", authorization);
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            var payload = new Dictionary<string, string>
+            {
+                { "programatic", "false" }
+            };
+
+            try
+            {
+                var response = httpClient.PostAsync(url, new FormUrlEncodedContent(payload)).Result;
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+                var jsonResponse = JObject.Parse(responseBody);
+                return jsonResponse["data"]["id"].ToString();
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"預留導入 ID 失敗：{e.Message}");
+                Environment.Exit(1);
+                return null;
+            }
+        }
+
+        public static void UploadFiles(string importId, string authorization, List<Dictionary<string, string>> finalImportFilesList, string dirPath)
+        {
+            string url = $"https://sleephq.com/api/v1/imports/{importId}/files";
+            using HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Add("Authorization", authorization);
+
+            foreach (var value in finalImportFilesList)
+            {
+                string osPath = value["path"].Trim();
+                string filePath = Path.Combine(osPath, value["name"]);
+
+                using (var content = new MultipartFormDataContent())
+                {
+                    ByteArrayContent fileContent = new(File.ReadAllBytes(filePath));
+                    content.Add(fileContent, "file", value["name"]);
+
+                    foreach (var kvp in value)
+                    {
+                        content.Add(new StringContent(kvp.Value), kvp.Key);
+                    }
+
+                    try
+                    {
+                        var response = httpClient.PostAsync(url, content).Result;
+                        response.EnsureSuccessStatusCode();
+                        Console.WriteLine($"File {value["name"]} have been imported to SleepHQ");
+                    }
+                    catch (HttpRequestException e)
+                    {
+                        Console.WriteLine($"Upload {value["name"]} failed：{e.Message}");
+                        Environment.Exit(1);
+                    }
+                }
+
+                Thread.Sleep(1500);
+            }
+        }
+
+        public static void ProcessImportedFiles(string importId, string authorization)
+        {
+            string url = $"https://sleephq.com/api/v1/imports/{importId}/process_files";
+            using HttpClient httpClient = new();
+            httpClient.DefaultRequestHeaders.Add("Authorization", authorization);
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            try
+            {
+                var response = httpClient.PostAsync(url, null).Result;
+                response.EnsureSuccessStatusCode();
+                Console.WriteLine($"File have been processed by SleepHQ with Import ID：{importId}");
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Failed to upload the file：{e.Message}");
+                Console.WriteLine($"You can try to import the files with this link：{url}");
+                Environment.Exit(1);
+            }
+        }
+
+        public static void CheckImportedFiles(string importId, string authorization)
+        {
+            string url = $"https://sleephq.com/api/v1/imports/{importId}";
+            using var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Authorization", authorization);
+            httpClient.DefaultRequestHeaders.Add("Accept", "application/json");
+
+            try
+            {
+                var response = httpClient.GetAsync(url).Result;
+                response.EnsureSuccessStatusCode();
+
+                var responseBody = response.Content.ReadAsStringAsync().Result;
+                var jsonResponse = JObject.Parse(responseBody);
+                string failedReason = jsonResponse["data"]["attributes"]["failed_reason"].ToString();
+
+                if (!string.IsNullOrEmpty(failedReason))
+                {
+                    Console.WriteLine($"Failed to process：{failedReason}");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Failed to check the file：{e.Message}");
+                Console.WriteLine($"You can try to import the files with this link：{url}");
+                Environment.Exit(1);
+            }
+        }
+    }
+}
